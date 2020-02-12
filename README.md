d829f8cf0163e91216219d313223c78d36551338
franciscolopezsancho <franciscolopezsancho@gmail.com>
2020-02-11 21:08:43 -0600

    custom value class: correctness

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ Maybe for the first commit.
 A value class is a class that will not allocate itself as an object at runtime. 
 must extend AnyVal or an universal trait, and have just one value field.
 Can only extend from universal traits ( UT extends Any, only defs, no inicialization )
+
+Correctness:
+
+while providing type checking you don't pay the overhead of object allocation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Add the jar from the beginning
--- src/main/scala/Bench.scala
+++ src/main/scala/Bench.scala
@@ -1,18 +1,24 @@
-object Implicits {
-  implicit class ValueClass(val value: Int) extends AnyVal {
+class ValueClass(val value: String) extends AnyVal {
+  def add(v: ValueClass): ValueClass =
+    new ValueClass(value + v.value)
+}
 
-    def toHex: String =
-      value.toString + "HOX"
-  }
+class Meter(val value: Double) extends AnyVal {
+    def +(m: Meter): Meter = new Meter(value + m.value)
 }
 
 class Bench {
 
-  import Implicits._
-
-
   def run: Unit =  {
-    println(2.toHex)
+    val vc1 = new ValueClass("hello ")
+    val vc2 = new ValueClass("world")
+
+    println(vc1 add vc2)
+
+    val x = new Meter(3.4)
+    val y = new Meter(4.3)
+    val z = x + y
+    println(z)
   }
 
 
